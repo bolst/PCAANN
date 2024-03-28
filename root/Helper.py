01e5b2b32e4cc0066b87c28a83d43b36faf933e9
bolst <bolton21@uwindsor.ca>
2024-03-28 14:19:51 -0400

    update requirements

--- root/Helper.py
+++ root/Helper.py
@@ -1,24 +1,33 @@
-import os, sys, subprocess
+import os
+import sys
+import subprocess
 import tkinter as tk
 from tkinter import filedialog
 
 CWD = os.path.dirname(os.path.realpath(__file__))
 
+
 def exec_script(name: str):
-    p = subprocess.Popen(['call', os.path.join(CWD, name)], stdout=sys.stdout, shell=True)
+    p = subprocess.Popen(['call', os.path.join(CWD, name)],
+                         stdout=sys.stdout, shell=True)
     p.communicate()
+
 
 def build():
     exec_script("build.bat")
 
+
 def run():
     exec_script("run.bat")
+
 
 def stop():
     exec_script("stop.bat")
 
+
 def update():
     exec_script("update.bat")
+
 
 def help():
     print("")
@@ -63,7 +72,8 @@ def driver(first_run=False):
         if file_path.endswith('.xlsx'):
             print(f'Selected file: {file_path}')
             file_path = file_path.replace('/', '\\')
-            os.system(f"copy \"{file_path}\" {os.path.join('PCA-ANN-code', 'RAW-DATA')}")
+            os.system(f"copy \"{file_path}\" {
+                      os.path.join('PCA-ANN-code', 'RAW-DATA')}")
             pass
         else:
             print('Error: data must be in .xlsx format')
@@ -72,6 +82,7 @@ def driver(first_run=False):
     elif user_input == 'exit':
         exit()
     driver()
+
 
 if __name__ == "__main__":
     driver(first_run=True)
